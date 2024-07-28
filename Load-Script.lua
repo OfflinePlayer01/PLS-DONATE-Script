3d2ad4d6d2a973f2da9de9b52b6aa58479d3f85d
OfflinePlayer01 <168975740+OfflinePlayer01@users.noreply.github.com>
2024-07-28 11:40:38 +0300

    Create Load-Script.lua

---
+++ Load-Script.lua
@@ -0,0 +1,17 @@
+--[[
+Script By _offlineplayer_#0000 | Script By _offlineplayer_#0000 | Script By _offlineplayer_#0000 | Script By _offlineplayer_#0000 | Script By _offlineplayer_#0000 | 
+Thank You For Using My Script | Thank You For Using My Script | Thank You For Using My Script | Thank You For Using My Script | Thank You For Using My Script | 
+]]--
+		    game:GetService("StarterGui"):SetCore("SendNotification", { 
+        Title = "〰 Script Made By _offlineplayer_ 〰";
+        Text = "〰 Roblox : 917Dream :) 〰";
+        Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+    Duration = 3;
+--Sends Notifications
+		    game:GetService("StarterGui"):SetCore("SendNotification", { 
+        Title = "〰 Pls Donate Script Loaded 〰";
+        Text = "〰 Enjoy :) 〰";
+        Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+    Duration = 3;
+--Opens Scripts
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OfflinePlayer01/Pls-Donate-Script/main/Copy.lua", true))()
